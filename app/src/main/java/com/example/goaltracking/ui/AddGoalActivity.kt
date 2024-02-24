93c44796c2768806cdac6336c5b69038543fa2a2
Nagendra Yadav <kumarnagendra2460@gmai.com>
2024-02-25 01:14:06 +0530

    work MainActivity

--- app/src/main/java/com/example/goaltracking/ui/AddGoalActivity.kt
+++ app/src/main/java/com/example/goaltracking/ui/AddGoalActivity.kt
@@ -3,10 +3,19 @@ package com.example.goaltracking.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.goaltracking.R
+import com.example.goaltracking.databinding.ActivityAddGoalBinding
+import com.example.goaltracking.model.Goal
 
 class AddGoalActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddGoalBinding
+    private lateinit var goal: Goal
+    private lateinit var oldGoal : Goal
+    var isUpdate = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding =
         setContentView(R.layout.activity_add_goal)
     }
 }
--- app/src/main/java/com/example/goaltracking/ui/MainActivity.kt
+++ app/src/main/java/com/example/goaltracking/ui/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.example.goaltracking.ui
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.LinearLayout
+import android.widget.PopupMenu
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.cardview.widget.CardView
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.goaltracking.R
 import com.example.goaltracking.adapter.GoalAdapter
 import com.example.goaltracking.database.GoalDatabase
@@ -10,13 +18,24 @@ import com.example.goaltracking.databinding.ActivityMainBinding
 import com.example.goaltracking.model.Goal
 import com.example.goaltracking.model.GoalViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), GoalAdapter.GoalClickListener, PopupMenu.OnMenuItemClickListener {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var database: GoalDatabase
     lateinit var viewModel: GoalViewModel
     lateinit var adapter: GoalAdapter
     lateinit var selectedGoal : Goal
+
+    private val updateGoal = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+        if(result.resultCode == Activity.RESULT_OK){
+
+            val goal = result.data?.getSerializableExtra("goal") as? Goal
+            if (goal != null){
+
+                viewModel.updateGoal(goal)
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -36,10 +55,54 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-
+         database = GoalDatabase.getDatabase(this)
     }
 
     private fun initUI() {
-        TODO("Not yet implemented")
+      binding.recyclerView.setHasFixedSize(true)
+        binding.recyclerView.layoutManager = StaggeredGridLayoutManager(2, LinearLayout.VERTICAL)
+        adapter = GoalAdapter(this, this)
+        binding.recyclerView.adapter = adapter
+
+        val getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
+            if (result.resultCode == Activity.RESULT_OK){
+               val goal = result.data?.getSerializableExtra("goal") as? Goal
+                if (goal != null){
+                    viewModel.insertGoal(goal)
+                }
+            }
+        }
+
+        // this method is use to FloatingActionButton
+
+        binding.fbAddGoal.setOnClickListener {
+            val intent = Intent(this, AddGoalActivity::class.java)
+            getContent.launch(intent)
+        }
+    }
+
+    override fun onItemClicked(goal: Goal) {
+        val intent = Intent(this@MainActivity, AddGoalActivity::class.java)
+        intent.putExtra("current_goal", goal)
+        updateGoal.launch(intent)
+    }
+
+    override fun onLongItemClicked(goal: Goal, cardView: CardView) {
+        selectedGoal = goal
+        popUpDisplay(cardView)
+    }
+    private fun popUpDisplay(cardView: CardView) {
+       val popup = PopupMenu(this, cardView)
+        popup.setOnMenuItemClickListener(this@MainActivity)
+        popup.inflate(R.menu.pop_up_menu)
+        popup.show()
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.delete_goal){
+            viewModel.deleteGoal(selectedGoal)
+            return true
+        }
+        return false
     }
 }
