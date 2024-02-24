6a2b67159617380def65fdfacab38118b2cf2f89
Nagendra Yadav <kumarnagendra2460@gmai.com>
2024-02-25 02:20:41 +0530

    AddGoalActivity_work

--- app/src/main/java/com/example/goaltracking/model/Goal.kt
+++ app/src/main/java/com/example/goaltracking/model/Goal.kt
@@ -2,13 +2,15 @@ package com.example.goaltracking.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.io.Serializable
 
 @Entity(tableName = "goals_table")
 data class Goal(
     @PrimaryKey(autoGenerate = true)
-    val id : Int,
-    val title : String,
-    val deadline : String,
-    val categories : String,
-    val description : String
-)
+    val id: Int?,
+    val title: String,
+    val deadline: String,
+    val categories: String,
+    val description: String,
+    val format: String
+): Serializable
--- app/src/main/java/com/example/goaltracking/ui/AddGoalActivity.kt
+++ app/src/main/java/com/example/goaltracking/ui/AddGoalActivity.kt
@@ -1,10 +1,15 @@
 package com.example.goaltracking.ui
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.goaltracking.R
+import android.widget.Toast
 import com.example.goaltracking.databinding.ActivityAddGoalBinding
 import com.example.goaltracking.model.Goal
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class AddGoalActivity : AppCompatActivity() {
 
@@ -13,9 +18,51 @@ class AddGoalActivity : AppCompatActivity() {
     private lateinit var oldGoal : Goal
     var isUpdate = false
 
+    @SuppressLint("SimpleDateFormat")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding =
-        setContentView(R.layout.activity_add_goal)
+        binding = ActivityAddGoalBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        try {
+            oldGoal = intent.getSerializableExtra("current_goal") as Goal
+            binding.etTitle.setText(oldGoal.title)
+            binding.etDeadline.setText(oldGoal.deadline)
+            binding.etCategories.setText(oldGoal.categories)
+            binding.etDescription.setText(oldGoal.description)
+            isUpdate = true
+        } catch (e : Exception){
+            e.printStackTrace()
+        }
+        binding.imgCheck.setOnClickListener {
+            val title = binding.etTitle.text.toString()
+            val deadline = binding.etDeadline.text.toString()
+            val categories = binding.etCategories.text.toString()
+            val description = binding.etDescription.text.toString()
+
+
+            if (title.isNotEmpty() || deadline.isNotEmpty() || categories.isNotEmpty() || description.isNotEmpty()){
+                val formatter = SimpleDateFormat("EEE, d MMM, yyyy HH:mm a")
+                if (isUpdate){
+                    goal = Goal(
+                        oldGoal.id,title, deadline, categories, description, formatter.format(Date())
+                    )
+                } else {
+                    goal = Goal(
+                        null, title, deadline, categories, description, formatter.format(Date())
+                    )
+                }
+                val intent = Intent()
+                intent.putExtra("goal", goal)
+                setResult(Activity.RESULT_OK, intent)
+                    finish()
+            } else {
+                Toast.makeText(this@AddGoalActivity, "Please enter some data", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+        }
+        binding.imgBack.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
