0177adb2ef26c800e2934f2f3ca5c8b1fa104841
Nagendra Yadav <kumarnagendra2460@gmai.com>
2024-02-27 00:44:20 +0530

    Add_calender

--- app/src/main/java/com/example/goaltracking/ui/AddGoalActivity.kt
+++ app/src/main/java/com/example/goaltracking/ui/AddGoalActivity.kt
@@ -1,15 +1,21 @@
 package com.example.goaltracking.ui
 
 import android.annotation.SuppressLint
-import android.app.Activity
+import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.EditText
 import android.widget.Toast
+import com.example.goaltracking.R
 import com.example.goaltracking.databinding.ActivityAddGoalBinding
 import com.example.goaltracking.model.Goal
 import java.text.SimpleDateFormat
+import java.util.Calendar
 import java.util.Date
+
 
 class AddGoalActivity : AppCompatActivity() {
 
@@ -18,11 +24,40 @@ class AddGoalActivity : AppCompatActivity() {
     private lateinit var oldGoal : Goal
     private var isUpdate = false
 
+    // Calender
+    private lateinit var dateView: EditText
+
     @SuppressLint("SimpleDateFormat")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddGoalBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val languages = resources.getStringArray(R.array.categories_item)
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_list, languages)
+        val autocompleteTV = findViewById<AutoCompleteTextView>(R.id.auto_complete)
+
+        autocompleteTV.setAdapter(arrayAdapter)
+
+
+
+        // This function is use to calender
+        dateView = findViewById(R.id.et_deadline)
+        dateView.setOnClickListener{
+            val calender = Calendar.getInstance()
+
+            val year = calender.get(Calendar.YEAR)
+            val month = calender.get(Calendar.MONTH)
+            val day = calender.get(Calendar.DAY_OF_MONTH)
+
+            val datePickerDialog = DatePickerDialog(this, { _, year, monthOfYear, dayOfMonth ->
+                    val date = (dayOfMonth.toString() + "." + (monthOfYear + 1) + "."  + year)
+                    dateView.setText(date)
+                },
+                year, month, day
+            )
+            datePickerDialog.show()
+        }
 
         try {
             oldGoal = intent.getSerializableExtra("current_goal") as Goal
@@ -54,15 +89,15 @@ class AddGoalActivity : AppCompatActivity() {
                 }
                 val intent = Intent()
                 intent.putExtra("goal", goal)
-                setResult(Activity.RESULT_OK, intent)
+                setResult(RESULT_OK, intent)
                     finish()
             } else {
-                Toast.makeText(this@AddGoalActivity, "Please enter some data", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@AddGoalActivity, "Please Enter Your Goal", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
         }
         binding.imgBack.setOnClickListener {
-            onBackPressed()
+            onBackPressedDispatcher.onBackPressed()
         }
     }
 }
